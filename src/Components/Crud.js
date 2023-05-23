65853dcb942fe1760620a0b9b2dec9cff3443b65
simon <simongouzien@hotmail.fr>
2023-05-23 09:37:38 +0200

    start creating

---
+++ src/Components/Crud.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Crud() {
+  return (
+    <div classname='container'>
+        <div className='row'>
+            <div className='col-6'>
+                <label>Name</label>
+                <input type='text' className='form-control Name' name='Name'></input>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Crud
